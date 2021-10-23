141c58a0eab159771ed9795a8267b32ef24aceaf
Justin Butler <57680670+JustinButler700@users.noreply.github.com>
2021-10-23 10:27:21 -0700

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -1,4 +1,8 @@
 //Justin Butler 10/23/2021
+/*
+0 ms, faster than 100.00% of Java online submissions for Find Minimum in Rotated Sorted Array II.
+Memory Usage: 38.5 MB, less than 93.69% of Java online submissions for Find Minimum in Rotated Sorted Array II.
+*/
 class Solution {
     public int findMin(int[] nums) {
         int output = Integer.MAX_VALUE;
